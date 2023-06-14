9282512caa91f49b3d08f41b45004afdc1ddb2b5
Oluwa-ios <50711478+oluwatobiHammed@users.noreply.github.com>
2023-06-14 21:04:21 +0100

    adjusted scroll inset

--- MovieExplorer/BaseViewController.swift
+++ MovieExplorer/BaseViewController.swift
@@ -26,7 +26,7 @@ class BaseViewController: UIViewController {
         $0.contentInsetAdjustmentBehavior = .never
         $0.keyboardDismissMode = .interactive
         $0.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 30, right: 0)
-        $0.scrollIndicatorInsets = UIEdgeInsets(top: 15, left: 0, bottom: 10, right: 0)
+        $0.scrollIndicatorInsets = UIEdgeInsets(top: 15, left: 0, bottom: 0, right: 0)
         $0.alwaysBounceVertical = true
         return $0
     }(UITableView())
